2f4ea504b81143db221d5c164bdc6201472faf64
Danigu78 <daniel.gonzalezu@estudiante.uam.es>
2026-03-30 16:13:52 +0200

    a

---
+++ PADSOF/src/tienda/tienda/FiltroSegundaMano.java
@@ -0,0 +1,20 @@
+package tienda;
+
+import productos.EstadoProducto;
+
+public class FiltroSegundaMano {
+
+	private double valorMinimo;
+	private double valorMaximo;
+	private EstadoProducto estadoMinimo;
+	
+	public FiltroSegundaMano() {
+		this.valorMinimo=0;
+		this.valorMaximo=Double.MAX_VALUE;
+		this.estadoMinimo
+	}
+	
+	
+	
+	
+}
